3e6b1635a1665e9cf0f853d116d2f629a8bb8e3f
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-01 17:34:57 -0600

    Day 1: Find the two numbers that add to 2020

---
+++ 2020/01/expense_report.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+
+#  ___               __  _
+# |   \ __ _ _  _   /  \/ |
+# | |) / _` | || | | () | |
+# |___/\__,_|\_, |  \__/|_|
+#            |__/
+#
+# "Fixing my expense report"
+
+# Challenge:
+# Specifically, they need you to find the two entries that sum to 2020 and then
+# multiply those two numbers together.
+
+mapfile -t LIST < expense_report.txt
+
+for A in ${LIST[@]}
+do
+  for B in ${LIST[@]}
+  do
+    # Pair of nested loops, so this will return the same answer twice
+    [ $(($A + $B)) -eq 2020 ] && echo "$A plus $B = $(($A + $B))"
+  done
+done
