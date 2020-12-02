6253efc02514cd28faabed94c00ad1c4edd82372
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-02 17:35:01 -0600

    Day 02: Wrap up a few of these weird lines into cleaner functions

--- 2020/02/password_validator.ps1
+++ 2020/02/password_validator.ps1
@@ -12,18 +12,42 @@
 # times (inclusive) in STRING.
 
 # Pull a sample row from the file to work on the test
-$row = "7-10 d: dddddddddpzwqflvdx"
+$example = "7-10 d: dddddddddpzwqflvdx"
 
-# Translate my description above into a regex that saves named matches.
-#   - You can name matches!
-#   - The -match will save this into $Matches variable automatically.
-#   - The -matches test will return T/F, which will output on the terminal if
-#     you don't capture it (hence Out-Null)
-$row -match '(?<Min>\d+)-(?<Max>\d+) (?<Char>[A-Za-z]): (?<Passwd>.+)' | Out-Null
+function ParseRow {
+  param([string]$row)
+  # Translate my description above into a regex that saves named matches.
+  #   - You can name matches!
+  #   - The -match will save this into $Matches variable automatically.
+  #   - The -matches test will return T/F, which will output on the terminal if
+  #     you don't capture it (hence Out-Null)
+  $row -match '(?<Min>\d+)-(?<Max>\d+) (?<Char>[A-Za-z]): (?<Passwd>.+)' | Out-Null
+  $Matches
+}
 
-$how_many = $($Matches.Passwd.ToCharArray() | Where-Object {$_ -eq $Matches.Char} | Measure-Object).Count
-"The test string contains the character $how_many times"
+function GetCount {
+  param(
+    [string]$Text,
+    [string]$Char
+  )
 
-if (($how_many -ge $Matches.Min) -and ($how_many -le $Matches.Max)) {
+  # Explode the string into an array, filter the test character, count it
+  $($Text.ToCharArray() | Where-Object {$_ -eq $Char} | Measure-Object).Count
+}
+
+function IsEnough {
+  param(
+    [int]$Count,
+    [int]$Min,
+    [int]$Max
+  )
+
+  (($Count -ge $Min) -and ($Count -le $Max))
+}
+
+$Values = ParseRow $example
+$Count = GetCount $Values.Passwd $Values.Char
+
+if (IsEnough $Count $Values.Min $Values.Max) {
   "The test string contains the character an acceptable number of times"
 }
