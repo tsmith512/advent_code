84f3846d006a0c8f9845c43b0bf40aced129b645
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-02 16:45:26 -0600

    Day 02: Test a regex

--- 2020/02/password_validator.ps1
+++ 2020/02/password_validator.ps1
@@ -1,5 +1,3 @@
-# No clue what to write this in yet.
-
 #  ___               __ ___
 # |   \ __ _ _  _   /  \_  )
 # | |) / _` | || | | () / /
@@ -12,3 +10,12 @@
 # Input file contains lines in this format: `LOW-HIGH CHAR: STRING`
 # Count lines from the input file where CHAR is present between LOW and HIGH
 # times (inclusive) in STRING.
+
+# Pull a sample row from the file to work on the test
+$row = "7-10 w: dpzwqflvdx"
+
+# Translate my description above into a regex that saves named matches
+# -match will save this into $Matches automatically.
+$row -match '(?<Min>\d+)-(?<Max>\d+) (?<Char>[A-Za-z]): (?<Passwd>.+)'
+
+$Matches
