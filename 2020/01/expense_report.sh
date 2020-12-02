e198c1e97368d3d559ae3ed56e81998758281877
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-02 00:39:39 -0600

    Day 01: Sort the list first, bail faster on impossible combos in Part Two. Reduces exec time from 38 seconds to 1 second!

--- 2020/01/expense_report.sh
+++ 2020/01/expense_report.sh
@@ -12,7 +12,12 @@
 # Specifically, they need you to find the two entries that sum to 2020 and then
 # multiply those two numbers together.
 
-mapfile -t LIST < expense_report.txt
+mapfile -t LIST < <(cat expense_report.txt | sort -n)
+  # NOTE: Sort the list so we can bail when numbers get high. This will save
+  # time on Part Two. This redirection worked but I had to research it. Piping
+  # the output of `sort` into `mapfile` didn't work because piped commands run
+  # in a subshell, so mapfile did work but the array was lost when the subshell
+  # terminated.
 
 for A in ${LIST[@]}
 do
@@ -38,9 +43,14 @@ for A in ${LIST[@]}
 do
   for B in ${LIST[@]}
   do
+    [ $(($A + $B)) -gt 2020 ] && break 1
+    # If A + B is already 2020+, bail out.
+
     for C in ${LIST[@]}
     do
       [ $(($A + $B + $C)) -eq 2020 ] && echo "$A + $B + $C = $(($A + $B + $C)). Multiplied: $(( $A * $B * $C ))" && break 3
     done
   done
 done
+
+# Solution: 591 + 1021 + 408 = 2020. Multiplied: 246191688
