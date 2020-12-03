24f332c5d87f98a45b711d8e31bc914cf2af2337
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-03 09:36:26 -0600

    Day 03: Part two with multiple slopes, but it's wrong somewhere because one of the slopes is a repeat but returns a different answer

--- 2020/03/avoid_trees.c
+++ 2020/03/avoid_trees.c
@@ -59,8 +59,51 @@ int main() {
 
   printf("\n");
 
-  printf("You crashed into %d trees.", count);
+  printf("You crashed into %d trees on the original slope.\n\n", count);
   // Part One Answer: You crashed into 159 trees.
+
+  //  ___          _     ___
+  // | _ \__ _ _ _| |_  |_  )
+  // |  _/ _` | '_|  _|  / /
+  // |_| \__,_|_|  \__| /___|
+  //
+  // This time multiple slopes are provided. Run each, capture the number of
+  // trees encountered on each, then return the product of the set.
+  int slopes[5][3] = {
+    /* {Y offset, X offset, number of trees} or "rise over run" */
+    {1, 1, 0},
+    {1, 3, 0},
+    {1, 5, 0},
+    {1, 7, 0},
+    {2, 1, 0}  /* Yes, down-two. */
+  };
+
+  count = 0;
+
+  for (int attempt = 0; attempt < 5; attempt++) {
+    for (y = 0; y < HEIGHT; y+= slopes[attempt][0]) {
+      if (field[y][x] == '#') {
+        count++;
+      }
+
+      // The for loop moves us down, this moves us over, then wrap around
+      x += slopes[attempt][1];
+      x = x % (WIDTH - 2);
+    }
+
+    // Save the number of trees we hit on this attempt
+    slopes[attempt][2] = count;
+
+    printf("You crashed into %d trees on the %d,%d slope.\n", count, slopes[attempt][0], slopes[attempt][1]);
+  }
+
+  // Now that we have each slope, multiply them together.
+  int tree_product = slopes[0][2];
+  for (int attempt = 1; attempt < 5; attempt++) {
+    tree_product *= slopes[attempt][2];
+  }
+
+  printf("The product of those numbers is %d\n\n", tree_product);
 
   return 0;
 }
