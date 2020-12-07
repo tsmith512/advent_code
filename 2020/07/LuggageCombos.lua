2c50d9c3b07943bfcafc4cd12a2a991bc1e2d9af
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-07 14:17:54 -0600

    Day 07: Finish splitting rules into child > parent relationships

--- 2020/07/LuggageCombos.lua
+++ 2020/07/LuggageCombos.lua
@@ -24,15 +24,69 @@
 
 INPUT = 'sample_rules.txt'
 
+-- Collect all puzzle rules into a table like this:
+-- rules[child-color][parent-color] == allowable-quantity
+-- See parserule() and parsechildren()
 rules = {}
 
-function parserule(rule)
-  print(rule)
-  parentcolor = string.match(rule, "(.+) bags contain")
-  childrule = string.match(rule, "(%d [^.,]+)[.,]?")
-  print(parentcolor)
-  print(childrule)
+
+-- Given a rule from the puzzle input
+-- (e.g. "clear tan bags contain 5 bright purple bags, 1 pale black bag, 5 muted lime bags.")
+-- Split that up into the Parent Color and a table of allowable child members
+-- Then insert the set into {rules} above.
+function parserule (rule)
+  local parentcolor, allowablechildren = string.match(rule, "(.+) bags contain (.+)")
+  local childrules = parsechildren(allowablechildren, {})
+
+  if (not isempty(childrules)) then
+    -- This parent bag can hold +1 child types, split and save them
+    for index, child in ipairs(childrules) do
+
+      -- Split the quantity from the color of the child member
+      local qty, childcolor = string.match(child, "(%d+) (.+) ?")
+
+      -- If rules[childcolor] is new, set up a table for it
+      if rules[childcolor] == nil then
+        rules[childcolor] = {}
+      end
+
+      rules[childcolor][parentcolor] = qty
+    end
+  else
+    -- @TODO: This parent can contain no other bags. I don't think this matters.
+  end
 end
+
+
+-- Given the allowable contents string from a puzzle input
+-- (e.g. "5 faded blue bags, 6 dotted black bags[...]")
+-- split it into its members by recursion
+function parsechildren (inputstring, collection)
+  if (inputstring == "no other bags.") then
+    return nil
+  end
+
+  -- What's the first child bag type?
+  local bag, remainder = string.match(inputstring, "(%d [^.,]+)bags[ .,]?(.+)")
+
+  -- Add this bag type to the collection
+  table.insert(collection, bag)
+
+  -- If remainder !empty: the parent bag can hold more, get the next one.
+  if (not isempty(remainder)) then
+    parsechildren(remainder, collection)
+  end
+
+  -- Return the list of bags types we know about
+  return collection
+end
+
+
+-- Simple utility to test of a var is empty or unset
+function isempty (input)
+  return input == nil or input == ""
+end
+
 
 -- Debug print of a potentially nested table.
 -- Thanks to JCH2k at StackOverflow https://stackoverflow.com/a/22460068
@@ -58,3 +112,5 @@ for rule in io.lines(INPUT) do
   parserule(rule)
   print("")
 end
+
+print(dump(rules))
