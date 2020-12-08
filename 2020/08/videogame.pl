9c4a6eb5f168fb290ed2109e9c8e44eeb94c9461
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-08 01:09:54 -0600

    Day 08: Ohhhh good.

---
+++ 2020/08/videogame.pl
@@ -0,0 +1,17 @@
+#!/bin/env perl
+
+#  ___               __  ___
+# |   \ __ _ _  _   /  \( _ )
+# | |) / _` | || | | () / _ \
+# |___/\__,_|\_, |  \__/\___/
+#            |__/
+#
+# "Handheld Halting"
+#
+# Challenge:
+# Given {accumulator = 0} a list of instructions:
+#   - nop: continue to the next step
+#   - acc (signed int): increase/decrease accumulator
+#   - jmp (signed int): advanced/rewind by int, resume execution
+# The instruction list is ultimately circular. Report accumulator value before
+# the infinite loop begins.
