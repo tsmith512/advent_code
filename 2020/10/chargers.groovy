06c4af08a652cf375b711a1eee313d5647b153ac
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-10 22:13:30 -0600

    Day 10: Collect inputs and what I think the puzzle description is

---
+++ 2020/10/chargers.groovy
@@ -0,0 +1,14 @@
+//  ___              _  __
+// |   \ __ _ _  _  / |/  \
+// | |) / _` | || | | | () |
+// |___/\__,_|\_, | |_|\__/
+//          |__/
+//
+// "Adapter Array"
+//
+// Challenge:
+// (This problem narrative was harder for me to parse. Here's what I think.)
+// The starting point is 0. Each number in the puzzle input represents a widget.
+// A widget can take an input 1, 2, or 3 lower than it is. Using every number
+// in the input, order them such that they fall in this pattern and collect the
+// distribution of increments between them.
