9b08d284fb1ece7601ade7a1a3edeac91ca57e50
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-11 21:12:21 -0600

    Day 11: Get problem description and sample inputs

---
+++ 2020/11/seatshuffling.r
@@ -0,0 +1,34 @@
+#  ___              _ _
+# |   \ __ _ _  _  / / |
+# | |) / _` | || | | | |
+# |___/\__,_|\_, | |_|_|
+#            |__/
+#
+# "Seating System"
+#
+# Challenge:
+# This is a cellular automaton problem essentially. Given a "seating chart" with
+# floor spaces (.), empty seats (L), and occupied seats (#), deduce the
+# arrangement given the input and these rules:
+# - "Adjacent" seats are all 8 surrounding a given seat (so, diagonals too)
+# - Transformation rules are applied simultaneously
+# - If a seat is Empty (L) without occupied seats (#) adjacent, it fills (#)
+# - If a seat is Occupied (#) and 4+ adj are also occupied (#), it empties (L)
+# - Otherwise, state does not change.
+# - Flooring is always just a floor.
+#
+# Here are the iterations of the sample chart:
+#
+# L.LL.LL.LL   #.##.##.##   #.LL.L#.##   #.##.L#.##   #.#L.L#.##   #.#L.L#.##
+# LLLLLLL.LL   #######.##   #LLLLLL.L#   #L###LL.L#   #LLL#LL.L#   #LLL#LL.L#
+# L.L.L..L..   #.#.#..#..   L.L.L..L..   L.#.#..#..   L.L.L..#..   L.#.L..#..
+# LLLL.LL.LL   ####.##.##   #LLL.LL.L#   #L##.##.L#   #LLL.##.L#   #L##.##.L#
+# L.LL.LL.LL   #.##.##.##   #.LL.LL.LL   #.##.LL.LL   #.LL.LL.LL   #.#L.LL.LL
+# L.LLLLL.LL   #.#####.##   #.LLLL#.##   #.###L#.##   #.LL#L#.##   #.#L#L#.##
+# ..L.L.....   ..#.#.....   ..L.L.....   ..#.#.....   ..L.L.....   ..L.L.....
+# LLLLLLLLLL   ##########   #LLLLLLLL#   #L######L#   #L#LLLL#L#   #L#L##L#L#
+# L.LLLLLL.L   #.######.#   #.LLLLLL.L   #.LL###L.L   #.LLLLLL.L   #.LLLLLL.L
+# L.LLLLL.LL   #.#####.##   #.#LLLL.##   #.#L###.##   #.#L#L#.##   #.#L#L#.##
+#
+# After repeated application of these rules, the seating distribution
+# stabilizes. Report how many seats are occupied at that point.
