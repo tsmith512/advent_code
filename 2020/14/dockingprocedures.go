ae06540b3480a812dbf8c02760f8ba2d381bf20a
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-15 00:28:42 -0600

    Day 14: Collect inputs

---
+++ 2020/14/dockingprocedures.go
@@ -0,0 +1,10 @@
+//  ___              _ _ _
+// |   \ __ _ _  _  / | | |
+// | |) / _` | || | | |_  _|
+// |___/\__,_|\_, | |_| |_|
+// 					 |__/
+//
+// "Docking Data"
+//
+// Challenge:
+// (To go to bed on time and come back tomorrow...)
