f256f47349e5542a3c88c5eac3cb43d91abd1041
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-16 23:59:37 -0600

    Day 15: Hello Ruby

---
+++ 2020/15/memory_game.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+=begin
+
+___              _ ___
+|   \ __ _ _  _  / | __|
+| |) / _` | || | | |__ \
+|___/\__,_|\_, | |_|___/
+           |__/
+
+"Rambunctious Recitation"
+
+Challenge:
+Transportation elves play a memory game that is a recitation of numbers.
+Starting from a given sequence (puzzle input), follow the rules and report the
+2020th number in the sequence.
+
+On a turn, the speaker considers the most recently spoken number, and:
+- If the number has not been spoken before, the player says 0.
+- Else, the player says how many turns ago the number was previously spoken.
+
+=end
+
+puts "Hello World"
