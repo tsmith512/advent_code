0a388a1000c573ab0ef28389a23e76cd6b869939
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-17 00:40:41 -0600

    Day 15: Wrong question, but be able to tell how long ago the input had been said

--- 2020/15/memory_game.rb
+++ 2020/15/memory_game.rb
@@ -16,9 +16,42 @@ Starting from a given sequence (puzzle input), follow the rules and report the
 2020th number in the sequence.
 
 On a turn, the speaker considers the most recently spoken number, and:
-- If the number has not been spoken before, the player says 0.
-- Else, the player says how many turns ago the number was previously spoken.
+- If the number had not been spoken before, the player says 0.
+- Else, the player says how many turns ago that number was previously spoken.
 
 =end
 
-puts "Hello World"
+class MemoryGame
+  @@said = []
+
+  def turn()
+    lookback = @@said.reverse
+    last = lookback.shift
+    puts "Starting a turn with #{last}"
+
+    if lookback.include?(last)
+      how_long = lookback.index(last)
+      puts "We'd said #{last} before #{how_long} turns ago / #{lookback}"
+      out = how_long
+    else
+      puts "New Number #{last} --> 0"
+      out = 0
+    end
+
+    @@said.push(out)
+    puts @@said
+    out
+  end
+
+  def start(input)
+    seq = input.split(',')
+    seq.each { |i| @@said.push(i.to_i) }
+    turn
+    turn
+    turn
+  end
+end
+
+
+game = MemoryGame.new
+game.start(File.read("memory_sample.txt").strip)
