6406cfdd7593b345f6b8ffe0bbeca4d69e06e0ed
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-21 20:29:31 -0600

    Day 18: Collect inputs. And PHP because I already lost twice tonight

---
+++ 2020/18/homework.php
@@ -0,0 +1,27 @@
+#!/usr/bin/env php
+<?php
+/*
+ ___              _ ___
+|   \ __ _ _  _  / ( _ )
+| |) / _` | || | | / _ \
+|___/\__,_|\_, | |_\___/
+           |__/
+
+"Operation Order"
+
+Challenge:
+Help a kid do his math homework. They broke PMDAS. Execute parentheses first,
+then all remaining operations left-to-right.
+
+Example:
+1 + (2 * 3) + (4 * (5 + 6))
+1 +      6  + (4 * (5 + 6))
+         7  + (4 * (5 + 6))
+         7  + (4 *     11)
+         7  +          44
+                       51
+
+Resolve each line in the input and report the sum.
+*/
+
+print "Hello World\n";
