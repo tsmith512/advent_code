d8dab5b9a5777f8a7a8f5dfe912d2a9219593561
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-22 01:00:40 -0600

    Day 18: Set up a resolver for addition first

--- 2020/18/homework.php
+++ 2020/18/homework.php
@@ -29,13 +29,23 @@ Examples:
                        51
 
 Resolve each line in the input and report the sum.
+
+ ___          _     ___
+| _ \__ _ _ _| |_  |_  )
+|  _/ _` | '_|  _|  / /
+|_| \__,_|_|  \__| /___|
+
+Challenge:
+
+Update to the math rules, parens are still resolved first, but then it's
+addition before multiplication.
 */
 
-mathHomework("homework_sample.txt");
+mathHomework("homework_sample.txt", 2);
 // Part One solution:
 //   Total sum 4491283311856
 
-function mathHomework($input) {
+function mathHomework($input, $puzzle) {
   $handle = fopen($input, "r");
   $total = 0;
 
@@ -44,11 +54,11 @@ function mathHomework($input) {
       $problem = trim($line);
       print "New problem: $problem\n";
 
-      $value = doProblem($problem);
+      $value = doProblem($problem, $puzzle);
       $total += $value;
 
-      print "Problem solution $value\n";
-      print "Total sum $total\n\n";
+      print "Problem solution $value\n\n\n";
+      // print "Total sum $total\n\n";
     }
   }
 }
@@ -57,9 +67,15 @@ function mathHomework($input) {
  * Accept a problem statement and solve it, if parentheses are encountered, get
  * them resolved by doParens.
  */
-function doProblem($problem) {
+function doProblem($problem, $puzzle) {
   while (strpos($problem, "(") !== false) {
-    doParens($problem);
+    doParens($problem, $puzzle);
+  }
+
+  if ($puzzle == 2) {
+    while (strpos($problem, "+") !== false) {
+      doAddition($problem, $puzzle);
+    }
   }
 
   print "Input: $problem\n";
@@ -75,13 +91,13 @@ function doProblem($problem) {
  * open-parent preceding it, and resolve that set by with doProblem and
  * splicing it back into the problem string.
  */
-function doParens(&$problem) {
+function doParens(&$problem, $puzzle) {
   if (($close = strpos($problem, ")")) !== false) {
     $length = strlen($problem);
     $open = strrpos($problem, "(", - $length + $close - 1);
 
     $subProblem = substr($problem, $open + 1, ($close - $open - 1));
-    $subSolution = doProblem($subProblem);
+    $subSolution = doProblem($subProblem, $puzzle);
 
     $problem = substr($problem, 0, $open) . $subSolution . substr($problem, $close + 1);
   }
@@ -111,4 +127,20 @@ function doStep($current, $piece) {
   }
 
   return $current;
+}
+
+/**
+ * Given a problem, resolve the first addition problem we find.
+ */
+function doAddition(&$problem) {
+  $matches = array();
+  preg_match('/(\d+) \+ (\d+)/', $problem, $matches);
+
+  $patchStart = strpos($problem, $matches[0]);
+  $patchLength = strlen($matches[0]);
+  $subSolution = intval($matches[1]) + intval($matches[2]);
+
+  print "  Addition $matches[0] --> $subSolution\n";
+  $problem = substr($problem, 0, $patchStart) . $subSolution . substr($problem, $patchStart + $patchLength);
+  print "    -> $problem\n";
 }
