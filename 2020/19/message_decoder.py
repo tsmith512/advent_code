f2e1855ef74cb6c8f42213fd88ea1dcf55a95ab1
Taylor Smith <tsmith@tsmithcreative.com>
2020-12-23 09:57:53 -0600

    Day 19: Oh my and it's all gone wrong. Delete it all.

--- 2020/19/message_decoder.py
+++ 2020/19/message_decoder.py
@@ -36,8 +36,6 @@ aaaabbb
 Report the number of given message that satisfy Rule 0.
 """
 
-import itertools
-
 INPUT_FILE = "message_sample.txt"
 
 def main():
@@ -45,8 +43,6 @@ def main():
 
   rulebook(rules)
   print(messages)
-
-  print(decode(rules, 0))
 
 # Slurp the input file and split it in half, then put the rules in an dict of
 # ID - RuleString and put the given messages in an array.
@@ -65,61 +61,6 @@ def setup(filename):
   messages = contents[1].split("\n")
 
   return rules, messages
-
-# Given the dict of rules and an ID to investivate, do that?
-def decode(rules, id):
-  # Split the rule text into options at the pipe: "2 3 | 3 2" -> ['2 3', '3 2']
-  rule = rules[id].split("|")
-  for half, option in enumerate(rule):
-
-    # Split the option into its parts by space: '2 3' -> [2, 3]
-    parts = option.strip().split(" ")
-    for index, part in enumerate(parts):
-
-      # If the part is a number, we need to decode it
-      if part.isnumeric():
-        innards = decode(rules, int(part))
-        print("Decoded {}: {}".format(part, innards))
-        parts[index] = resolve(innards)
-
-    rule[half] = parts[0] if len(parts) == 1 else parts
-  # By now `rule` contains 1 or 2 (the pipe) options to satisfy the rule.
-  return rule[0] if len(rule) == 1 else rule
-
-# Somewhere in here is a good idea but I think I walked too far away from it
-# because it stopped making sense. Idea:
-# - If we get an array with one member, unbox that and return it.
-# - If we get two strings of 1 letter each, combine them.
-# - If we get lists, we need to resolve all combinations they could create
-# - Otherwise, return the input unchanged (string input)
-def resolve(input):
-  print("Resolving: {}".format(input))
-
-  if type(input) == list and len(input) == 1:
-    return input[0]
-
-  # I think we can just join strings once they've been multiplied?
-  elif all(type(x) == str and len(x) == 1 for x in input):
-    return "".join(input)
-
-  # If we have a list of lists, we should get the cartesian product
-  elif all(type(x) == list for x in input):
-    product = list(itertools.product(*input))
-    combos = ["".join(combo) for combo in product]
-    # @HELP! ^^ This line currently fails when resolving Rule 1 on the sample
-    # input because `combo` is a list instead of a string. I think I'm doing
-    # this in the wrong place. This is the last print:
-
-    # Decoded 1: [[['ab', 'ab', 'ab', 'ab'], ['ab', 'aa', 'bb', 'ba']], [['ab', 'aa', 'bb', 'ba'], ['ab', 'ab', 'ab', 'ab']]]
-    # Resolving: [[['ab', 'ab', 'ab', 'ab'], ['ab', 'aa', 'bb', 'ba']], [['ab', 'aa', 'bb', 'ba'], ['ab', 'ab', 'ab', 'ab']]]
-
-    # Which I think is what Rule 1 boils down to, but somewhere a product didn't
-    # get executed.
-
-    print(combos)
-    return combos
-  else:
-    return input
 
 # Pretty printer for the rules dict
 def rulebook(rules):
