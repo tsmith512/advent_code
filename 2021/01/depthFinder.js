2ef6aa1beea910ba5e8bdbf76cf38828c5958e3e
Taylor Smith <tsmith@cloudflare.com>
2021-12-01 12:45:00 -0600

    Day 01: Part 1!

--- 2021/01/depthFinder.js
+++ 2021/01/depthFinder.js
@@ -27,3 +27,7 @@ const count = depths.reduce(
   (count, reading, index) => count + ((reading > depths[index - 1]) ? 1 : 0), 0);
 
 console.log(`Of those, ${count} readings increased over the previous value.`);
+
+// Part One:
+// There are 2000 readings total.
+// Of those, 1711 readings increased over the previous value.
