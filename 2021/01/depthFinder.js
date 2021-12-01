e54c690aa4af740f600fbe00662d8f7f80817709
Taylor Smith <tsmith@cloudflare.com>
2021-12-01 11:20:23 -0600

    Day 01: Hello, World 2021

---
+++ 2021/01/depthFinder.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+/**
+ *  ___               __  _
+ * |   \ __ _ _  _   /  \/ |
+ * | |) / _` | || | | () | |
+ * |___/\__,_|\_, |  \__/|_|
+ *            |__/
+ *
+ * "Sonar Sweep"
+ *
+ * Challenge;
+ * Given a list of numbers, count those which are higher than their predecessor.
+ */
+
+console.log('Hello Advent 2021');
