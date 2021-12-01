da2ff42222d8d0cbc45503a38f1e28ecf9d0561f
Taylor Smith <tsmith@cloudflare.com>
2021-12-01 11:38:53 -0600

    Day 01: Work with sample data and a simple reducer

--- 2021/01/depthFinder.js
+++ 2021/01/depthFinder.js
@@ -13,4 +13,29 @@
  * Given a list of numbers, count those which are higher than their predecessor.
  */
 
-console.log('Hello Advent 2021');
+const depths = [
+  199,
+  200,
+  208,
+  210,
+  200,
+  207,
+  240,
+  269,
+  260,
+  263,
+];
+
+console.log(`There are ${depths.length} readings total.`)
+
+const count = depths.reduce((count, reading, index, all) => {
+
+  // Is this one higher than the last one?
+  if (reading > all[index - 1]) {
+    return count + 1;
+  }
+
+  return count;
+}, 0);
+
+console.log(`Of those, ${count} readings increased over the previous value.`);
