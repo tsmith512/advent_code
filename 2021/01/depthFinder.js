27e05a54c49bf90b3c5e4e8bf4cfd2a18e20c90d
Taylor Smith <tsmith@tsmithcreative.com>
2021-12-01 23:38:10 -0600

    Day 01: Part 2

--- 2021/01/depthFinder.js
+++ 2021/01/depthFinder.js
@@ -23,11 +23,41 @@ const depths = fs.readFileSync('part1_input.txt')
 
 console.log(`There are ${depths.length} readings total.`)
 
-const count = depths.reduce(
-  (count, reading, index) => count + ((reading > depths[index - 1]) ? 1 : 0), 0);
+const countIncreases = (input) => input.reduce(
+  (count, current, index) => count + ((current > input[index - 1]) ? 1 : 0), 0);
+
+const count = countIncreases(depths);
 
 console.log(`Of those, ${count} readings increased over the previous value.`);
 
 // Part One:
 // There are 2000 readings total.
 // Of those, 1711 readings increased over the previous value.
+
+/**
+ *  ___          _     ___
+ * | _ \__ _ _ _| |_  |_  )
+ * |  _/ _` | '_|  _|  / /
+ * |_| \__,_|_|  \__| /___|
+ *
+ * Repeat the process but look for increases in the sum of three-measurement
+ * windows: "the number of times the sum of measurements in this sliding window
+ * increases from the previous sum"
+ */
+
+// Make a "window of 3" from the given index. Empty if we're out of numbers.
+// That's an okay assumption because we're looking for increases.
+const window = (n) => depths.length > (n + 2) ? depths.slice(n, n + 3) : [];
+
+// Sum values in an array
+const sum = (arr) => arr.reduce((sum, x) => sum + x, 0);
+
+// Build an array of 3-reading window totals
+const readings = depths.map((val, ind) => sum(window(ind)));
+
+const count2 = countIncreases(readings);
+
+console.log(`And over windows of three readings, ${count2} increased from the window prior.`);
+
+// Part Two:
+// And over windows of three readings, 1743 increased from the window prior.
