a8aa2cd7939c916594a9cb271338ea9ff3ebc2a4
Taylor Smith <tsmith@tsmithcreative.com>
2021-12-02 00:13:01 -0600

    Day 02: Simple instruction-follower with sample input

---
+++ 2021/02/dive.lua
@@ -0,0 +1,39 @@
+#!/usr/bin/env lua
+
+--  ___               __ ___
+-- |   \ __ _ _  _   /  \_  )
+-- | |) / _` | || | | () / /
+-- |___/\__,_|\_, |  \__/___|
+--            |__/
+--
+-- "Dive!"
+--
+-- Challenge: From `depth = 0` and `horizontal = 0', follow given instructions
+-- to a specific position. Then return the product of depth * horizontal.
+--
+-- 'up' -> depth--
+-- 'down' -> depth++
+-- 'forward' -> horizontal++ (there's no backing up in Part 1)
+-- (no left/right in Part 1)
+
+INPUT = 'sample.txt'
+
+DEPTH = 0
+HORIZ = 0
+
+function navigate (input)
+  local direction, distance = string.match(input, "(%a+) (%d+)")
+
+  if     direction == "up"      then DEPTH = DEPTH - distance
+  elseif direction == "down"    then DEPTH = DEPTH + distance
+  elseif direction == "forward" then HORIZ = HORIZ + distance
+  else print(string.format("Unknown rule %s", input))
+  end
+end
+
+for rule in io.lines(INPUT) do
+  navigate(rule)
+end
+
+print(string.format("Position: Depth = %d, Horizontal = %d", DEPTH, HORIZ))
+print(string.format("Product of position components: %d", DEPTH * HORIZ))
