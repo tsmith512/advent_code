afe12c0bc46ce2d3bbfb6142d0a76ceb4dff0c7b
Taylor Smith <tsmith@tsmithcreative.com>
2021-12-04 21:37:14 -0600

    Day 03: Part 2, first half with sample input... oof

--- 2021/03/binarydiagnostic.r
+++ 2021/03/binarydiagnostic.r
@@ -13,7 +13,7 @@
 # - "Epsilon rate" = the leas common digit for each index of input numbers.
 
 # Read the the input into a matrix
-lines <- scan("input.txt", what = "")
+lines <- scan("sample.txt", what = "")
 rows <- length(lines)
 cols <- length(unlist(strsplit(lines[1], "")))
 readings <- matrix(
@@ -55,3 +55,49 @@ sprintf("Power Consumption: %d", gamma * epsilon)
 # [1] "Gamma: 2576"
 # [1] "Epsilon: 1519"
 # [1] "Power Consumption: 3912944"
+
+
+#  ___          _     ___
+# | _ \__ _ _ _| |_  |_  )
+# |  _/ _` | '_|  _|  / /
+# |_| \__,_|_|  \__| /___|
+#
+# Essentually, reduce the matrix by iterating over columns and keeping rows with
+# the most (least) common value for that column, using 1 (0) as a tie-breaker to
+# find the Oxygen (CO2) sensor values.
+
+# Given a matrix, a column index, and what to do in the event of a tie, return
+# the most common value in that column.
+# for Oxygen Sensor, tie = 1
+# for CO2 Sensor, tie = 0
+commonColumn <- function(matrix, i, tie) {
+  a <- sum(matrix[,i]) / dim(matrix)[1]
+  if      (a > 0.5) { 1   }
+  else if (a < 0.5) { 0   }
+  else            { tie }
+}
+
+# Given a matrix, a column index, and what to do in the event of a tie, return
+# a matrix with the only the rows containing the most common value at that index.
+dropRows <- function(matrix, i, tie) {
+  keep <- commonColumn(matrix, i, tie)
+  matrix[ matrix[, i] == keep, ]
+}
+
+# Given a matrix and what to do in the event of a tie, reduce the matrix to only
+# that value which had the most common value for each column index.
+reduceSensorMatrix <- function(matrix, tie) {
+  for (col in 1:dim(matrix)[1]) {
+    # If there's only one row left, we have our answer.
+    if (!is.matrix(matrix)) break
+    print(matrix)
+
+    # Drop rows that don't contain the most common value for this column.
+    matrix <- dropRows(matrix, col, tie)
+  }
+
+  matrix
+}
+
+oxygen <- reduceSensorMatrix(readings, 1)
+print(Reduce(unbin, oxygen))
