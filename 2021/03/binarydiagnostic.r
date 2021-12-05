25bb92f9420be6c01669ccb1b818bf5518c9db19
Taylor Smith <tsmith@tsmithcreative.com>
2021-12-04 21:45:25 -0600

    Day 03: Part 2 part 2 with sample input

--- 2021/03/binarydiagnostic.r
+++ 2021/03/binarydiagnostic.r
@@ -66,38 +66,51 @@ sprintf("Power Consumption: %d", gamma * epsilon)
 # the most (least) common value for that column, using 1 (0) as a tie-breaker to
 # find the Oxygen (CO2) sensor values.
 
-# Given a matrix, a column index, and what to do in the event of a tie, return
-# the most common value in that column.
+# Given a matrix, a column index, whether to keep the most or least common value,
+# and what to do in the event of a tie, return the most/least  common value in
+# that column.
 # for Oxygen Sensor, tie = 1
 # for CO2 Sensor, tie = 0
-commonColumn <- function(matrix, i, tie) {
+commonColumn <- function(matrix, i, minmax, tie) {
   a <- sum(matrix[,i]) / dim(matrix)[1]
-  if      (a > 0.5) { 1   }
-  else if (a < 0.5) { 0   }
-  else            { tie }
+  if (a == 0.5) { tie }
+  else if (minmax == 1) {
+    if      (a > 0.5) { 1   }
+    else if (a < 0.5) { 0   }
+  }
+  else if (minmax == 0) {
+    if      (a > 0.5) { 0   }
+    else if (a < 0.5) { 1   }
+  }
 }
 
-# Given a matrix, a column index, and what to do in the event of a tie, return
-# a matrix with the only the rows containing the most common value at that index.
-dropRows <- function(matrix, i, tie) {
-  keep <- commonColumn(matrix, i, tie)
+# Given a matrix, a column index, whether to keep the most or least common value,
+# and what to do in the event of a tie, return a matrix with only the rows
+# containing the most/least common value at that index.
+dropRows <- function(matrix, i, minmax, tie) {
+  keep <- commonColumn(matrix, i, minmax, tie)
   matrix[ matrix[, i] == keep, ]
 }
 
-# Given a matrix and what to do in the event of a tie, reduce the matrix to only
-# that value which had the most common value for each column index.
-reduceSensorMatrix <- function(matrix, tie) {
+# Given a matrix, whether to keep most or least common values, and what to do in
+# the event of a tie, reduce the matrix to only that value which had the
+# most/least common value for each column index.
+reduceSensorMatrix <- function(matrix, minmax, tie) {
   for (col in 1:dim(matrix)[1]) {
     # If there's only one row left, we have our answer.
     if (!is.matrix(matrix)) break
-    print(matrix)
 
     # Drop rows that don't contain the most common value for this column.
-    matrix <- dropRows(matrix, col, tie)
+    matrix <- dropRows(matrix, col, minmax, tie)
   }
 
   matrix
 }
 
-oxygen <- reduceSensorMatrix(readings, 1)
-print(Reduce(unbin, oxygen))
+binOxygen <- reduceSensorMatrix(readings, 1, 1)
+oxygen <- Reduce(unbin, binOxygen)
+sprintf("Oxygen Sensor: %d", oxygen)
+
+binCo2 <- reduceSensorMatrix(readings, 0, 0)
+co2 <- Reduce(unbin, binCo2)
+sprintf("CO2 Sensor: %d", co2)
