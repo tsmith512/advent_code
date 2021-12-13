87a21efc01f9293ab4e58189739bc4e665d99375
Taylor Smith <tsmith@tsmithcreative.com>
2021-12-12 22:31:47 -0600

    Day 11: Set up a sequence that does the simple demo correctly... thanks @duplico

---
+++ 2021/11/octopus.r
@@ -0,0 +1,51 @@
+#  ___              _ _
+# |   \ __ _ _  _  / / |
+# | |) / _` | || | | | |
+# |___/\__,_|\_, | |_|_|
+#            |__/
+#
+# "Dumbo Octopus"
+# Given a field of octopuses (octopi?)
+
+# Read the the input into a matrix
+lines <- scan("short.txt", what = "")
+rows <- length(lines)
+cols <- length(unlist(strsplit(lines[1], "")))
+octos <- matrix(
+  data = as.numeric(unlist(strsplit(lines, ""))),
+  nrow = rows,
+  ncol = cols,
+  byrow = TRUE
+)
+
+
+flash <- function(matrix) {
+  # Increment each by one
+  matrix <- apply(matrix, c(1,2), function(value) value + 1)
+
+  # Who's fixin' to flash?
+  flashes <- apply(matrix, c(1,2), function(value) if (value > 9) { 1 } else { 0 })
+
+  # Thank you @duplico for this handy thing you did for 2020 Day 11.
+  # We know each adjacent point to a flash increases, and that these overlap.
+  # So make 8 matrixes
+  n  <- rbind(flashes[-1,], rep(0, cols))
+  s  <- rbind(rep(0, cols), flashes[-rows,])
+  e  <- cbind(flashes[,-1], rep(0, rows))
+  w  <- cbind(rep(0, rows), flashes[,-cols])
+  ne <- cbind(rbind(flashes[-1,], rep(0, cols))[,-1], rep(0, rows))
+  se <- cbind(rbind(rep(0, cols), flashes[-rows,])[,-1], rep(0, rows))
+  nw <- cbind(rep(0, rows), rbind(flashes[-1,], rep(0, cols))[,-cols])
+  sw <- cbind(rep(0, rows), rbind(rep(0, cols), flashes[-rows,])[,-cols])
+
+  # Apply these increases
+  matrix <- matrix + n + s + e + w + ne + se + nw + sw
+
+  apply(matrix, c(1,2), function(value) if (value > 9) { 0 } else { value })
+}
+
+for (i in 1:3) {
+  octos <- flash(octos)
+  print(i)
+  print(octos)
+}
