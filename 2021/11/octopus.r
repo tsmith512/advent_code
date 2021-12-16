711afcde6f2d5df1a48e941ad2c807cc937f5b00
Taylor Smith <tsmith@tsmithcreative.com>
2021-12-16 01:05:05 -0600

    Day 11: Part two solution. That was surprisingly straightforward.

--- 2021/11/octopus.r
+++ 2021/11/octopus.r
@@ -5,7 +5,12 @@
 #            |__/
 #
 # "Dumbo Octopus"
-# Given a field of octopuses (octopi?)
+# Given a field of octopuses (octopi?) each represented by a number 1 to 9. On
+# each iteration, increase each by 1. When an octopus reaches 10, it "flashes"
+# and increases each adjacent (diagonals included) octopus by +1 and resets to
+# 0. This cascades; if the flash brings a surrounding octopus to > 9, it flashes
+# too. Resolve each before moving on to the next round. How many total "flashes"
+# will have happened by round #100?
 
 # Read the the input into a matrix
 lines <- scan("input.txt", what = "")
@@ -46,11 +51,11 @@ flash <- function(matrix) {
 
   # And if anything is high enough to flash, flash it.
   if (sum(matrix > 9)) {
-    # print(matrix)
     matrix <- flash(matrix)
   }
 
-  # Anything that flashed is now a zero.
+  # Anything that flashed is now a zero again. Because we can only flash once
+  # per iteration, so if anyhting was affected in the recursion, reset it.
   matrix[flashes == 1] <- 0
 
   matrix
@@ -58,17 +63,34 @@ flash <- function(matrix) {
 
 totalFlashes <- 0
 
-for (i in 1:100) {
+#  ___          _     ___
+# | _ \__ _ _ _| |_  |_  )
+# |  _/ _` | '_|  _|  / /
+# |_| \__,_|_|  \__| /___|
+#
+# Instead of going to 100, repeat until all octos are back to zero. How long?
+# So swap `for i in 1:100` for `while all-octos > 0` but still output #100.
+
+totalRounds <- 0
+
+while (sum(octos) > 0) {
+  if (totalRounds == 100) {
+    print(sprintf('Total flashes by round 100: %d', totalFlashes))
+  }
+
   # Increment each by one.
   octos <- apply(octos, c(1,2), function(value) value + 1)
+  totalRounds <- totalRounds + 1
 
   octos <- flash(octos)
-  print('')
-  print(sprintf('After round %d', i))
-  print(octos)
 }
 
-print(sprintf('Total flashes: %d', totalFlashes))
+print(sprintf('After round %d, all octos were at zero.', totalRounds))
+print(sprintf('Total flashes after round %d: %d', totalRounds, totalFlashes))
 
 # Part One:
 # [1] "Total flashes: 1655"
+
+# Part Two:
+# [1] "After round 337, all octos were at zero."
+# [1] "Total flashes after round 337: 5352"
