0feebe708ec9711a78a98a8d50dfa50d2b5f49c7
Taylor Smith <tsmith@tsmithcreative.com>
2021-12-17 23:29:26 -0600

    Day 12: Set up part two but logic is incorrect

--- 2021/12/caving.js
+++ 2021/12/caving.js
@@ -16,7 +16,7 @@
  */
 
 const fs = require('fs');
-const input = fs.readFileSync('input.txt').toString().trim().split('\n');
+const input = fs.readFileSync('short.txt').toString().trim().split('\n');
 
 // Map out which caves lead to which caves.
 const paths = {};
@@ -40,7 +40,7 @@ input.forEach((pathway) => {
 
 // Map out paths from start to end (or dead ends)
 const routes = [];
-const traverse = (start = 'start', route = ['start']) => {
+const traverse = (start = 'start', route = ['start'], partTwo = false) => {
   // Where can we go from here, per the rules?
   const options = paths[start]
     .filter(x => {
@@ -53,8 +53,21 @@ const traverse = (start = 'start', route = ['start']) => {
       } else if (x === x.toUpperCase()) {
         // Upper Case caves can be revisited.
         return true;
+      } else if (partTwo) {
+        /**
+         *  ___          _     ___
+         * | _ \__ _ _ _| |_  |_  )
+         * |  _/ _` | '_|  _|  / /
+         * |_| \__,_|_|  \__| /___|
+         *
+         * You can visit any one lower-case cave a second time. Each route may
+         * contain no more than one lower-case cave twice.
+         */
+        return route.filter(y => y === x).length < 2;
+        // @TODO: ^^ Solves the wrong problem. This checks to see if _this_ cave
+        // has been seen more than once. Need to see if _any_ have.
       } else {
-        // Lower Case caves can only be visited one.
+        // FOR PART ONE: Lower Case caves can only be visited one.
         return !route.includes(x);
       }
     });
@@ -74,14 +87,19 @@ const traverse = (start = 'start', route = ['start']) => {
     if (next === 'end') {
       routes.push(thisPath);
     } else {
-      traverse(next, thisPath);
+      traverse(next, thisPath, partTwo);
     }
   });
-}
+};
 
 traverse();
 
-console.log(`There are ${routes.length} route through the caves.`);
+console.log(`There are ${routes.length} routes through the caves.`);
 
 // Part One:
 // There are 4775 route through the caves.
+
+routes.length = 0;
+traverse('start', ['start'], true);
+console.log(routes.map(r => r.join('-')));
+console.log(`And ${routes.length} routes if you double-back once.`);
