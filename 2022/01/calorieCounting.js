18519a37b93834e60eac1e9ecce02e25892eeb69
Taylor Smith <tsmith@tsmithcreative.com>
2022-12-02 01:29:02 -0600

    Day 01: Sample input on Day 1 for Calorie Counting

---
+++ 2022/01/calorieCounting.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+/**
+ * ___               __  _
+ * |   \ __ _ _  _   /  \/ |
+ * | |) / _` | || | | () | |
+ * |___/\__,_|\_, |  \__/|_|
+ *            |__/
+ *
+ * "Calorie Counting"
+ *
+ * Given a list of numbers where numbers are separated by new lines, and groups
+ * are seprated by blank lines, find the group with the highest count.
+ */
+
+
+const fs = require('fs');
+
+// Return the sum of an array of numbers
+const sum = (arr) => arr.reduce((total, next) => total + next, 0);
+
+// Find the largest number in an array of numbers
+const max = (arr) => arr.reduce((max, next) => (next > max ? next : max), 0);
+
+// Read the groupings into an array of snack packs (int[][])
+const elves = fs.readFileSync('sample.txt')
+  .toString()
+  .trim()
+  .split("\n\n")
+  .map(string => string.split("\n").map(n => parseInt(n)));
+
+// Sum each elf's snack pack into its total calories for the pack (int[])
+const calories = elves.map(e => sum(e));
+
+const maxCals = max(calories);
+
+console.log(`The highest elf snack-pack calorie count is ${maxCals}`);
