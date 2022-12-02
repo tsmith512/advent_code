13cebbc1c9218963aa2d97e86422528e17d1d706
Taylor Smith <tsmith@tsmithcreative.com>
2022-12-02 16:52:59 -0600

    Day 02: Part 2 sample

--- 2022/02/rock_paper_scissors.go
+++ 2022/02/rock_paper_scissors.go
@@ -23,7 +23,7 @@ import (
 	"os"
 )
 
-const filename string = "input.txt"
+const filename string = "sample.txt"
 
 const ROCK = 1
 const PAPER = 2
@@ -62,6 +62,41 @@ func main() {
 
 	// Part One: Total score: 11603
 	fmt.Printf("Total score: %d\n", total)
+
+  //  ___          _     ___
+  // | _ \__ _ _ _| |_  |_  )
+  // |  _/ _` | '_|  _|  / /
+  // |_| \__,_|_|  \__| /___|
+	//
+	// Actually Column 2 describes not _what_ you should play, but how you should
+	// make the round end.
+	//
+	// X = Play to lose
+	// Y = Draw
+	// Z = Play to win
+
+	file.Seek(0, 0)
+	scanner = bufio.NewScanner(file)
+	total = 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Their move uses the same logic.
+		them := moveCodes[string(line[0])]
+
+		// My move is now determined by how I'm supposed to play
+		me, intention := how(them, string(line[2]))
+
+		// And this is the same from Part 1
+		points, explaination := score(them, me)
+		total += uint64(points)
+
+		// But include the intention anyway.
+		fmt.Printf("%-20s %-8s vs %8s -> %-7s %d points.\n", intention, label(them), label(me), explaination, points)
+	}
+
+	fmt.Printf("Total score, part 2: %d\n", total)
 }
 
 func score (them int, me int) (points int, explain string) {
@@ -85,4 +120,27 @@ func score (them int, me int) (points int, explain string) {
 
 func label (move int) string {
 	return moveNames[move - 1]
+}
+
+func how (them int, goal string) (me int, explain string) {
+	// Modulo in Go doesn't work on negative ints, so bump this up so we end up
+	// with 3, 4, or 5, which (x % 3) => 0, 1, or 2.
+	them += 2
+
+	switch goal {
+	case "X":
+		explain = "Intentionally lose."
+		me = (them - 1) % 3
+	case "Y":
+		explain = "Seek a draw."
+		me = them % 3
+	case "Z":
+		explain = "Play to win."
+		me = (them + 1) % 3
+	}
+
+	// The existing scoring function expects `me` to be a 1-based code
+	me += 1
+
+	return
 }
