640359db819ae9c5dd2ad223bca671da5be68cd1
Taylor Smith <tsmith@cloudflare.com>
2022-12-07 15:26:24 -0600

    Day 07: Build a tree of File and Directory structs and a function to examine them

--- 2022/07/disk_space.go
+++ 2022/07/disk_space.go
@@ -19,9 +19,79 @@ import (
 	"os"
   "regexp"
   "strconv"
+	"strings"
 )
 
 const filename string = "sample.txt"
+
+type dirNode struct {
+	parent *dirNode
+	subdirs []*dirNode
+	files []*fileNode
+	name string
+}
+
+type fileNode struct {
+	parent *dirNode
+	name string
+	size int
+}
+
+func newRoot(name string) *dirNode {
+	dir := dirNode{name: name}
+	return &dir
+}
+
+func (d *dirNode) newDir(name string) *dirNode {
+	dir := dirNode{parent: d, name: name}
+	d.subdirs = append(d.subdirs, &dir)
+	return &dir
+}
+
+func (d *dirNode) newFile(name string, size int) *fileNode {
+	file := fileNode{name: name, size: size, parent: d}
+	d.files = append(d.files, &file)
+	return &file
+}
+
+func (d *dirNode) dir(name string) *dirNode {
+	for _, dir := range d.subdirs {
+		if dir.name == name {
+			return dir
+		}
+	}
+
+	return nil
+}
+
+func (d *dirNode) file(name string) *fileNode {
+	for _, file := range d.files {
+		if file.name == name {
+			return file
+		}
+	}
+
+	return nil
+}
+
+func (d *dirNode) examine(args ...int) {
+	var indent int
+	if (len(args) > 0) {
+		indent = args[0]
+	} else {
+		indent = 0
+	}
+
+	fmt.Printf("%s- %s/\n", strings.Repeat(" ", indent), d.name)
+
+	for _, sub := range(d.subdirs) {
+		sub.examine(indent + 2)
+	}
+
+	for _, file := range(d.files) {
+		fmt.Printf("%s- %s (%d)\n", strings.Repeat(" ", indent + 2), file.name, file.size)
+	}
+}
 
 func main() {
 	file, err := os.Open(filename)
@@ -31,7 +101,11 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	// We need to keep track of where we are...
-	var cd []string
+	var workingPath []*dirNode
+	var cd *dirNode
+
+	root := newRoot("C:")
+	cd = root
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -45,38 +119,52 @@ func main() {
 
 				if dir == "/" {
 					// Root directory
-					cd = []string{"/"}
+					workingPath = []*dirNode{root}
 				} else if dir == ".." {
 					// Back up one
-					cd = cd[:len(cd) - 1]
+					workingPath = workingPath[:len(workingPath) - 1]
 				} else {
-					// Go into a new one
-					cd = append(cd, dir)
+					// Go into a directory that...
+
+					if cd.dir(dir) != nil {
+						// ... already exists
+						workingPath = append(workingPath, cd.dir(dir))
+					} else {
+						// ... doesn't exist
+						workingPath = append(workingPath, cd.newDir(dir))
+					}
 				}
 
-				fmt.Println("cd to ", cd)
+				// Now that workingPath is updated, set the cd pointer too
+				cd = workingPath[len(workingPath) - 1]
+
 			case "ls":
-				fmt.Println("ls of ", cd)
+				// Ignore this case for now.
 			}
 		} else {
-			// This is `ls` output
+			// This is `ls` output denoting a new...
 			var filename string
 			if string(line[0:3]) == "dir" {
-				// It's a directory
+				// ... directory: add it to the tree
 				filename = string(line[4:])
-				fmt.Printf("Directory %s\n", filename)
+				cd.newDir(filename)
 			} else {
+				// ... file: get its size and add it to the tree
 				rex := `(\d+) (.+)`
 				captures := regexp.MustCompile(rex).FindStringSubmatch(line)
 				filename = captures[2]
 				filesize, err := strconv.Atoi(captures[1])
 
+				// Sure why not
 				if err != nil {
 					panic(err)
 				}
 
-				fmt.Printf("File %s of size %d\n", filename, filesize)
+				cd.newFile(filename, filesize)
 			}
 		}
   }
+
+	// With the full tree assembled, pretty-print it
+	root.examine()
 }
