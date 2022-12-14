073a9b592591889311862ef73c89269155c0805c
Taylor Smith <tsmith@cloudflare.com>
2022-12-14 17:59:40 -0600

    Day 10: Only report what we're asked for

--- 2022/10/crt.go
+++ 2022/10/crt.go
@@ -36,14 +36,18 @@ func main() {
 	register := 1 // Current "addx" value
 	i := 1 // Current step
 
-	window := []int{0}// List of what we're gonna have to add
+	window := []int{0} // List of what we're gonna have to add
+	checking := []int{20, 60, 100, 140, 180, 220} // Cycles to report on
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		fmt.Printf("Cycle %d: ", i)
-		fmt.Printf("register is %d", register)
-		// ^^ instruction/example says "during cycle" is this value
+		print := contains(checking, i)
+
+		if print {
+			fmt.Printf("Cycle %d: register is %d and signal strength is %d.\n", i, register, i * register)
+			// ^^ instruction/example says "during cycle" is this; before addx
+		}
 
 		if string(line[0:4]) == "noop" {
 			window = append(window, 0)
@@ -59,15 +63,33 @@ func main() {
 		register += window[0]
 		window = window[1:]
 		i += 1
-
-		fmt.Printf(" - next value %d\n", register)
 	}
 
+	// Now we have to run out any remaining instructions in the window, continuing
+	// the cycle count above but without input. Since we're running a range over
+	// window, I'm gonna just index it with j rather than shift it (although that
+	// did work correctly, it seems weird to mutate a slice while iterating it.)
 	for j := range window {
-		fmt.Printf("Cycle %d (runout): ", i)
-		fmt.Printf("register is %d", register)
+		print := contains(checking, i)
+
+		if print {
+			fmt.Printf("Cycle %d: register is %d and signal strength is %d.\n", i, register, i * register)
+			// ^^ instruction/example says "during cycle" is this; before addx
+		}
+
 		register += window[j]
 		i += 1
-		fmt.Printf(" - next value %d\n", register)
 	}
+}
+
+// So https://stackoverflow.com/a/71181131 says this is in an experimental lib
+// available now, but I do try to see how far I can get with the stdlib first
+func contains(h []int, n int) bool {
+	for _, x := range h {
+		if x == n {
+			return true
+		}
+	}
+
+	return false
 }
