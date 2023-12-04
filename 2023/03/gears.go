3a529d78112cb1004c122e5af8984bc1f02951a7
Taylor Smith <tsmith@tsmithcreative.com>
2023-12-03 21:16:32 -0600

    Day 03: Part 2 sample

--- 2023/03/gears.go
+++ 2023/03/gears.go
@@ -21,11 +21,15 @@ import (
 	"strings"
 )
 
-const FILENAME = "input.txt"
-const DEBUG = false
+const FILENAME = "sample.txt"
+const DEBUG = true
 
 var reNumber = regexp.MustCompile(`\d+`)
 var reSymbols = regexp.MustCompile(`[^0-9\.]`)
+var reAsterisk = regexp.MustCompile(`\*`)
+
+// ^^ This is dumb, but writing a strings.Index() wrapper that supports
+// multiple results would be annoying and not a lot faster at this scale.
 
 // Simple wrapper for debug printing
 func debugPrint(template string, data ...interface{}) {
@@ -128,4 +132,54 @@ func main() {
 	// Sum of these numbers: 539713
 	fmt.Printf("Count of part numbers: %v\n", len(realPartNumbers))
 	fmt.Printf("Sum of these numbers: %d\n", sumRealPartNumbers)
+
+	//
+	//  ___          _     ___
+	// | _ \__ _ _ _| |_  |_  )
+	// |  _/ _` | '_|  _|  / /
+	// |_| \__,_|_|  \__| /___|
+	//
+	// Now we need to find the "gears" -- an asterisk which is adjacent to exactly
+	// two part numbers. The "gear ratio" is the product of those two adjacent
+	// numbers; report the sum of all the ratios.
+
+	// Find the row and col for each asterisk in the schematic:
+	var allGears [][]int
+	sumGearRatios := 0
+
+	for row, data := range schematic {
+		gearLocations := reAsterisk.FindAllStringSubmatchIndex(data, -1)
+
+		for _, cols := range gearLocations {
+			allGears = append(allGears, []int{row, cols[0]})
+		}
+	}
+
+	debugPrint("\nFound gears at coords:\n%v\n", allGears)
+
+	// Cycle through each and see if there are exactly two neighboring part numbers.
+	for _, gear := range allGears {
+		debugPrint("\nSearching around %v\n", gear)
+		var neighbors []int
+
+		for _, part := range allParts {
+			top := part[1] - 1
+			bottom := part[1] + 1
+			left := part[2] - 1
+			right := part[3] + 1
+
+			if top <= gear[0] && gear[0] <= bottom && left <= gear[1] && gear[1] < right {
+				debugPrint("Gear at %v is a neighbor to %v\n", gear, part)
+				neighbors = append(neighbors, part[0])
+			}
+		}
+
+		if len(neighbors) == 2 {
+			ratio := neighbors[0] * neighbors[1]
+			debugPrint("Ratio: %d\n", ratio)
+			sumGearRatios += ratio
+		}
+	}
+
+	fmt.Printf("\nSum of all gear ratios: %d\n", sumGearRatios)
 }
