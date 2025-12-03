a0543483118318f579210a3b1244d6da586a468c
Taylor Smith <tsmith@tsmithcreative.com>
2025-12-03 12:01:51 -0600

    Day 02 Part 1 Sample in TypeScript

---
+++ 2025/02/giftshop.ts
@@ -0,0 +1,61 @@
+//  ___               __ ___
+// |   \ __ _ _  _   /  \_  )
+// | |) / _` | || | | () / /
+// |___/\__,_|\_, |  \__/___|
+//            |__/
+//
+// "Gift Shop"
+//
+// (On Node v20+ you can just run `npx tsx giftship.ts` to execute)
+//
+// Given a list of ranges /(\d+-\d+,)+/ (one long line), find "invalid" IDs:
+// range boundaries where a number/sequence is repeated twice. (As the whole
+// number-string?)
+//
+// > So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+//
+// "None of the ranges have leading zeroes" (so... may need to strip out?)
+
+import fs from 'fs';
+
+const DEBUG = true;
+const INPUT = 'sample.txt';
+
+const debugPrint = (input: any) => {
+  if (DEBUG) {
+    console.log(input);
+  }
+};
+
+// An invalid ID is a repeated sequence of digits, so split the string in half
+// and compare. This also filters out (as valid) odd-length strings silently.
+const isInvalid = (i: string) => i.slice(0, i.length / 2) === i.slice(i.length / 2)
+
+// Read the ranges into an array (str[])
+const ranges = fs.readFileSync(INPUT)
+  .toString()
+  .trim()
+  .split(",");
+
+let invalidCount = 0;
+
+// Isolate the invalid ranges:
+const invalid = ranges
+  // Expand ranges into the included numbers
+  .map(r => {
+    const [a, b] = r.split('-').map(i => parseInt(i));
+    return Array.from({ length: b - a + 1}, (_, i) => a + i);
+  })
+  // Flatten nested array to check them all
+  .flat()
+  // Convert them back to strings, which also would remove leading zeroes
+  .map(x => x.toString())
+  // Filter to keep only invalid strings.
+  .filter(i => isInvalid(i));
+
+debugPrint(invalid);
+console.log(`In provided ranges, there are ${invalid.length} invalid IDs.`);
+
+const sum = invalid.reduce((total: number, current: string) => total + parseInt(current), 0);
+
+console.log(`The sum of invalid IDs is: ${sum}`);
