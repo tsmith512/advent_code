d702d64e609eeec31b543678683fb28016fa0ed9
Taylor Smith <tsmith@tsmithcreative.com>
2025-12-06 16:37:32 -0600

    Day 06 Part 2 sample

--- 2025/06/trashcompactor.ts
+++ 2025/06/trashcompactor.ts
@@ -15,7 +15,7 @@
 import fs from 'fs';
 
 const DEBUG = true;
-const INPUT = 'input.txt';
+const INPUT = 'sample.txt';
 
 const debugPrint = (input: any) => {
   if (DEBUG) {
@@ -67,3 +67,59 @@ debugPrint(answers);
 // Part One:
 // The sum of all homework answers is 4076006202939.
 console.log(`The sum of all homework answers is ${op["+"](answers)}.`);
+
+//
+// PART TWO
+//
+// LOL OKAY MATH IS STUPIDER NOW.
+// Numbers are written top-to-bottom, right-to-left.
+//
+
+// Let's start over shall we?
+problems.length = 0;
+
+// Read and clean up the input. but different.
+const lines = fs.readFileSync(INPUT)
+  .toString()
+  .trim()
+  .split('\n');
+
+// We already know the operators.
+lines.pop();
+
+// Keep track of what problem we're populating
+let currentProblem = 0;
+problems.push([]);
+
+// From right to left...
+for (let i = lines[0].length; i >= 0; i--) {
+  // From top to bottom...
+  const digits: string[] = lines.map(l => l[i]);
+
+  // If a column is all spaces, we've moved on to a new problem.
+  // Make an array for it and continue to the next column.
+  if (digits.every(c => c === ' ')) {
+    currentProblem++;
+    problems.push([]);
+    continue;
+  }
+
+  const number: number = digits
+    // Cast to int, empty spaces will be NaN
+    .map(n => parseInt(n))
+    // Remove NaN's: both [NaN, NaN, 1] and [1, NaN, NaN] are 1, not 1 and 100.
+    .filter(n => !Number.isNaN(n))
+    // Put the full numbers together (re-using this reducer from Day 3 "Lobby"!)
+    // NB: 'n' is the result of the filter, 'number''s length may have changed:
+    .reduce((total, current, index, n) => total + (current * 10 ** (n.length - index - 1)), 0)
+
+  problems[currentProblem].push(number);
+}
+
+// These were read left-to-right, so reverse them.
+operators.reverse();
+
+// This part works the same way.
+const newAnswers = problems.map((n, i) => (op[operators[i]])(n));
+
+console.log(`Having learned to read octopus math, new sum is ${op["+"](newAnswers)}.`);
